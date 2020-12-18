3a68d78cab49cd0b84f84696f0ad33640131af1b
hugoh92 <hugoholiveira45@gmail.com>
2020-12-18 00:19:02 -0300

    Update main.c
    atualização e comentario

--- main.c
+++ main.c
@@ -1,69 +1,67 @@
-// ALGORITMO WELSH POWELL:
-
 #include <stdio.h>
 #include <stdbool.h>
+#define numVertices 10 
 
-#define pneu 11 //numero de vertices
 
-int pintar = 1;
-int graus[pneu];
-int cor[pneu];
-char vertices[pneu] = {'A','B','C','D','E','F','G','H','I','J','K'};
-int matrix[pneu][pneu] = {
-  {0,1,0,0,0,0,0,1,0,0,0},
-  {1,0,0,1,0,0,0,0,0,0,0},
-  {0,0,0,1,0,0,0,0,0,0,0},
-  {0,1,1,0,0,0,0,0,1,0,1},
-  {0,0,0,0,0,1,0,0,0,0,1},
-  {0,0,0,0,1,0,1,0,0,0,0},
-  {0,0,0,0,0,1,0,1,0,0,1},
-  {1,0,0,0,0,0,1,0,1,1,1},
-  {0,0,0,1,0,0,0,1,0,1,0},
-  {0,0,0,0,0,0,0,1,1,0,1},
-  {0,0,0,1,1,0,1,1,0,1,0}
+int graus[numVertices];
+int colorir = 1;
+int cor[numVertices];
+char vertices[numVertices] = {'A','B','C','D','E','F','G','H','I','J'}; //vertices
+int matrix[numVertices][numVertices] = { 
+  {0,1,0,0,0,0,0,1,0,0},
+  {1,0,0,1,0,0,0,0,0,0},
+  {0,0,0,1,0,0,0,0,0,0},
+  {0,1,1,0,0,0,0,0,1,0},
+  {0,0,0,0,0,1,0,0,0,0},
+  {0,0,0,0,1,0,1,0,0,0},
+  {0,0,0,0,0,1,0,1,0,0},
+  {1,0,0,0,0,0,1,0,1,1},
+  {0,0,0,1,0,0,0,1,0,1},
+  {0,0,0,0,0,0,0,1,1,0}
 };
 
-void achaGr4ul(){
-  for(int a = 0; a < pneu; a++)
-    for(int k = 0; k < pneu; k++)
+
+int colore(int pColore){
+  for(int j = 0; j < numVertices; j++)
+    if(matrix[pColore][j])
+      if(cor[j] == colorir)
+        return colorir + 1;
+  return colorir;
+}
+
+void grau(){ //função para achar grau do vertice
+  for(int a = 0; a < numVertices; a++)
+    for(int k = 0; k < numVertices; k++)
       if(matrix[a][k] == 1)
         graus[a]++;
 }
-
-int pintarV(int caraParaPintar){
-  for(int j = 0; j < pneu; j++)
-    if(matrix[caraParaPintar][j])
-      if(cor[j] == pintar)
-        return pintar + 1;
-  return pintar;
-}
-
-void welshPowell(){
+void welshPowell(){ //algoritmo de welsPowell
   int v, aux;
   do{
     aux = 0;
-    for(int a = 0; a < pneu; a++) // buscando o vertice com o menor total e não visitado ainda
-      if(cor[a] == 0)
+    for(int a = 0; a < numVertices; a++) // Buscando vertice com menor total e que não foi visitado ainda
+      if(cor[a] == 0) // se cor igual a 0
         if(graus[a] > aux) {
           v = a;
           aux = graus[a];
         }
     if(cor[v] == 0)
-      cor[v] = pintar;
-    else break; //todos os vertices estão pintados
-    for(int y = 0; y < pneu; y++)
+      cor[v] = colorir; //colore
+    else break; //caso vertics ja foram pintados
+    for(int y = 0; y < numVertices; y++)
       if(matrix[v][y] == 0 && cor[y] == 0)
-        cor[y] = pintarV(y);
-    pintar++;
+        cor[y] = colore(y);
+    colorir++; //contador
   } while(true);
 }
 
+
 int main(){
-  for(int g = 0; g < pneu; g++)
+  for(int g = 0; g < numVertices; g++)
     cor[g] = graus[g] = 0;
-  achaGr4ul(); // funcção do GR4UL
+  grau(); 
   welshPowell();
-  for(int a = 0; a < pneu; a++){
+  for(int a = 0; a < numVertices; a++){
     printf("vertice(%c), ", vertices[a]);
     if(cor[a] == 1) printf("cor: azul\n");
     else if(cor[a] == 2) printf("cor: cinza\n");
